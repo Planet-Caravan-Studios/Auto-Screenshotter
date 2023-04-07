c7bd59b29c8a1f4a77cbab2b74353842a665de7f
Alec Reimel <alecreimel1@gmail.com>
2023-04-06 20:13:35 -0400

    more fully built out prototype version

--- screenshot.js
+++ screenshot.js
@@ -2,7 +2,10 @@ const puppeteer = require('puppeteer');
 const url = process.argv[2];
 const fileName = process.argv[3];
 if (!url) {
-    throw "Please provide URL as a first argument";
+    throw "Please provide URL as the first argument";
+}
+if (!fileName) {
+    throw "Please provide fileName as the second argument";
 }
 async function run () {
     const browser = await puppeteer.launch();
@@ -13,7 +16,10 @@ async function run () {
       deviceScaleFactor: 1,
     });
     await page.goto(url);
-    await page.screenshot({path: 'screenshot-files/' + fileName + '.png', fullPage: true});
+    await page.screenshot({
+    	path: 'screenshot-files/' + fileName + '.png', 
+    	fullPage: true,
+    });
     browser.close();
 }
 run();
